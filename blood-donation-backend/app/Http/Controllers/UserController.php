7baf1555f2c3d68439bc8be91420f4079f9985a5
Neon17 <neon.neupane.9@gmail.com>
2025-07-07 16:55:17 +0545

    refactor: refactoring code on controller: UserController

--- blood-donation-backend/app/Http/Controllers/UserController.php
+++ blood-donation-backend/app/Http/Controllers/UserController.php
@@ -64,7 +64,7 @@ class UserController extends Controller
     public function getDashboardActivity(Request $request)
     {
         $user = $request->user();
-        $activities = [];
+        $activities = [] ;
 
         // Get user's blood requests
         $requests = BloodRequest::where('user_id', $user->id)
